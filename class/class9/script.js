9c9c27c7d4cc1178f241204ff2c36e8f5c150aa8
Masha-bit <mashachristopherifechukwude@gmail.com>
2023-02-21 00:26:16 +0000

    added new classes

---
+++ class/class9/script.js
@@ -0,0 +1,16 @@
+//data type
+//type casing
+
+let x ='franc'
+
+let name ='Masha'
+name = 'Christopher'
+
+let age = '17   '
+const phoneNumber = '09044314423'
+
+let aboutMe= `My name is ${name} and i am ${age} years old /n call me on ${phoneNumber}`
+
+alert(aboutMe)
+
+document.print(aboutMe)
